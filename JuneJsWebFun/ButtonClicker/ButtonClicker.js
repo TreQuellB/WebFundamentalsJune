8a7ec275b9f6b64e9267d1816ffc8a9b0c056789
TreQuellB <trebelle1@gmail.com>
2022-06-27 22:54:49 -0400

    update

---
+++ JuneJsWebFun/ButtonClicker/ButtonClicker.js
@@ -0,0 +1,10 @@
+function loggingOut(element) {
+    element.innerText = "Logout";
+}
+function custom() {
+    // we can include more code here if we want to
+    console.log("this message is coming from js.");
+}
+function add_definition(element) {
+    element.remove();
+}
